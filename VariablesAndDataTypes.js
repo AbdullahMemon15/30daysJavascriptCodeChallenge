ad083758f74a246620373286cd0e0bac0f14bddd
AbdullahMemon15 <130421052+AbdullahMemon15@users.noreply.github.com>
2024-07-14 21:18:32 -0700

    Create VariablesAndDataTypes.js
    DAY 1 Challenge Completed

---
+++ VariablesAndDataTypes.js
@@ -0,0 +1,40 @@
+// Task 1
+var count = 2
+console.log(count)
+
+// Task 2
+let student = 'Abdullah'
+console.log(student)
+
+// Task 3
+const istrue =  true
+console.log(istrue);
+
+// Task 4
+let numOfTasks = 3
+let teacher = 'hasnain'
+let isOld = false
+let objectOfStudent = {
+    firstName: 'Abdullah',
+    lastName: 'Akhund',
+    age:24,
+}
+let arrayOfTasks = ['Cook','clean','mop']
+
+console.log(typeof(numOfTasks))
+console.log(typeof(teacher))
+console.log(typeof(isOld))
+console.log(typeof(objectOfStudent))
+console.log(typeof(arrayOfTasks))
+
+
+//task 5
+
+let empolyee = 'abdullah'
+console.log('empolyee name is ' + empolyee);
+empolyee = "eshaan"
+console.log('empolyee name is now ' + empolyee);
+
+//task 6
+// istrue = false
+
